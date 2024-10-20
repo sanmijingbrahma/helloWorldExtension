c15167a44aed6989e4faf81877fc2e6953dfc9a5
sanmijingbrahma <bsanmijing@gmail.com>
2024-10-20 11:54:42 +0530

    Add functionalities with js and change the icons

--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
 const btn = document.querySelector("#btn1")
 const text = document.querySelector("#responseText")
 
+let clickCount = 0;
+// Using Chrome's local storage
+
+chrome.storage.local.get(['clickCount'],(result)=>{
+
+    if(result.clickCount){
+        clickCount = result.clickCount
+    }
+    text.textContent = `Hello World ${clickCount}`
+
+})
+
+
 btn.addEventListener("click",()=>{
-    text.textContent = "Button Clicked!"
+    clickCount++
+    text.textContent = `Hello World ${clickCount}`
+})
+
+chrome.storage.local.set({clickCount:clickCount},()=>{
+    console.log("Successfully saved!");
+    
 })
